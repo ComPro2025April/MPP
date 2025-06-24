85709582bbc398930f75f35b8e2d118d87d545be
undrakhorgil <undrakhorgil.o@gmail.com>
2025-06-24 10:47:11 -0500

    Delete src directory

--- src/lesson2/lecture/dependency_example/RightTriangle.java
+++
@@ -1,9 +0,0 @@
-package lesson2.lecture.dependency_example;
-
-public class RightTriangle{
-    public double computeHypotenuseLength(double base, double height){
-        //The class, RightTriangle uses Math class only when
-        //computeHypotenuseLength() is called
-        return Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
-    }
-}
