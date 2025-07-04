bd79a4c250ab5559f891198998487fc62f0fab5c
Undrakh Orgil Odonkhuu <uodonkhuu@miu.edu>
2025-07-04 16:56:11 -0500

    Lab 9

--- lesson9/lab/prob2/Main.java
+++ lesson9/lab/prob2/Main.java
@@ -2,9 +2,13 @@ package lesson9.lab.prob2;
 
 public class Main {
     public static void main(String[] args) {
-        Expr expr = new Multiplication(new Addition(new Constants(2), new Constants(3)), new Constants(4));
+        // (2+3)∗4 = 20
+        Expr expr1 = new Multiplication(new Addition(new Constants(2), new Constants(3)), new Constants(4));
+        System.out.println(eval(expr1));
 
-        System.out.println(eval(expr));
+        // (2+3)∗(4*5) = 100
+        Expr expr2 = new Multiplication(new Addition(new Constants(2), new Constants(3)), new Multiplication(new Constants(4), new Constants(5)));
+        System.out.println(eval(expr2));
     }
 
     public static int eval(Expr expr) {
